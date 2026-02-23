5499e0a8d6373903dbd57fcd36c47a7b493fa235
Om005 <chavdaom84@gmail.com>
2026-02-24 01:00:35 +0530

    add: basic nginx configuration and image

--- server/docker/make_images.sh
+++ server/docker/make_images.sh
@@ -4,5 +4,7 @@ docker build -t dokit-node:latest ./node/
 docker build -t dokit-express:latest ./express/
 # Build React+Vite image
 docker build -t dokit-react_vite:latest ./react_vite/
+# Build Nginx image
+docker build -t dokit-nginx:latest ./nginx/
 # Verify they exist
 docker images | grep dokit-
